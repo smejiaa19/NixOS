f29e9a40d4d3942cf3a58137af46164c35f66e27
smejiaa19 <silviomejiagarcia@gmail.com>
2025-10-16 07:50:58 -0600

    Agregando poetry

--- shells/python/shell.nix
+++ shells/python/shell.nix
@@ -5,10 +5,12 @@ pkgs.mkShell{
 		pkgs.python312
 		pkgs.python312Packages.pip
 		pkgs.python312Packages.virtualenv
+		(pkgs.poetry.override { version = "1.3.2"; })
 	];
 
 	shellHook = ''
 		echo " Hola chaval estamos usando la piton"
 		python3 --version
+		poetry --version
 		'';
 }
